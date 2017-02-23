62240e1f813334037691c39efe154cb1ad838299
Alex Kornitzer <alex.kornitzer@countercept.com>
2017-02-23 17:50:13 +0000

    A horrible fix for a locking issue

--- i3/.i3/i3extra.sh
+++ i3/.i3/i3extra.sh
@@ -12,7 +12,7 @@ fi
 # Start autolock
 if [[  $(pgrep -cx xautolock) -eq 0 ]] ; then
   locker='~/.i3/i3lock.sh'
-  xautolock -time 1 -notify 5 -notifier "notify-send 'Locking'" -corners 0-00 -locker $locker &
+  xautolock -time 1 -detectsleep -notify 5 -notifier "notify-send 'Locking'" -corners -000 -locker $locker &
 fi
 
 # Start RedShift GTK
--- i3/.i3/i3lock.sh
+++ i3/.i3/i3lock.sh
@@ -6,8 +6,12 @@ if [[  $(pgrep -cx i3lock) -gt 0 ]] ; then
   exit 1
 fi
 
+# FIXME: The hackiest hack, make sure we escape VMWare Workstation as locking
+# seems to fail otherwise!!!
+xdotool keydown ctrl+alt; xdotool keyup ctrl+alt
+
 # Lock screen displaying this image.
 i3lock --color "#151515"
 
 # Turn the screen off after a delay.
-sleep 60; pgrep i3lock && xset dpms force off
+(sleep 60; pgrep i3lock && xset dpms force off) &
