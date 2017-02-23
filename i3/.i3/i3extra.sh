362d67048968c87c0293df36d38690931fa79c55
Alex Kornitzer <alex.kornitzer@countercept.com>
2017-02-23 11:39:23 +0000

    Added i3extra.sh
    This is used to prevent the launching of programs only needed on certain
    configs.

---
+++ i3/.i3/i3extra.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# Executions that need some if statements!
+#
+
+# Exit if a we are a virtual machine
+if [[  $(pgrep -cx vmgfx) -gt 0 ]] ; then
+  vmware-user-suid-wrapper &
+  exit
+fi
+
+# Start autolock
+if [[  $(pgrep -cx xautolock) -eq 0 ]] ; then
+  locker='~/.i3/i3lock.sh'
+  xautolock -time 1 -notify 5 -notifier "notify-send 'Locking'" -corners 0-00 -locker $locker &
+fi
+
+# Start RedShift GTK
+if [[  $(pgrep -cx redshift-gtk) -eq 0 ]] ; then
+  redshift-gtk &
+fi
