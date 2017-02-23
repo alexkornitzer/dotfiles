c770ca16a94645e93e9dc39d7f2b947a0e107f8b
Alex Kornitzer <alex.kornitzer@countercept.com>
2017-02-23 21:15:07 +0000

    Added screenlayouts

---
+++ i3/.i3/screenlayout/1-2-3.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --auto --pos 0x0 --rotate normal --output DP1 --auto --right-of eDP1 --rotate normal --output HDMI2 --off --output HDMI1 --off --output DP2 --auto --right-of DP1 --rotate normal
