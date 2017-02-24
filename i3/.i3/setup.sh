21164e6b46f435008be962884fb8e05122a19935
Alex Kornitzer <alex.kornitzer@countercept.com>
2017-02-24 13:21:06 +0000

    Trying out TLP

--- i3/.i3/setup.sh
+++ i3/.i3/setup.sh
@@ -2,5 +2,7 @@
 
 # Very basic script to ensure that dependencies are installed
 sudo apt-get install i3 i3lock i3blocks arc-theme compton dunst fonts-dejavu \
-  fonts-font-awesome moka-icon-theme redshift-gtk rofi thunar udiskie \
-  xfce4-power-manager xfce4-terminal
+  fonts-font-awesome moka-icon-theme redshift-gtk rofi thunar tlp udiskie \
+  xfce4-terminal
+
+# FIXME: Don't use TLP in a VM!!!
