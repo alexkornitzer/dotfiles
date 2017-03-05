3ac8b6e40d6b907d082d26dea304b006b5743e33
Alex Kornitzer <Alex@Alex-Kornitzers-MacBook-Pro.local>
2017-03-05 17:34:28 +0000

    Slightly better setup.sh

--- i3/.i3/setup.sh
+++ i3/.i3/setup.sh
@@ -2,7 +2,9 @@
 
 # Very basic script to ensure that dependencies are installed
 sudo apt-get install i3 i3lock i3blocks arc-theme compton dunst fonts-dejavu \
-  fonts-font-awesome moka-icon-theme redshift-gtk rofi thunar tlp udiskie \
-  xfce4-terminal
+  fonts-font-awesome moka-icon-theme rofi thunar udiskie xfce4-terminal
 
 # FIXME: Don't use TLP in a VM!!!
+if ! [[ "$(lsmod | grep vmwgfx)" ]]; then
+  sudo apt-get install redshift-gtk tlp
+fi
