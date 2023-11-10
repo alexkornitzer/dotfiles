c4a60b061bd9a26eeb349f07967c397474935c37
Alex Kornitzer <alex.kornitzer@withsecure.com>
2023-11-10 16:00:47 +0000

    feat(neovim): add guard

---
+++ neovim/.config/nvim/lua/plugins/guard.lua
@@ -0,0 +1,18 @@
+-- Guard: async fast minimalist plugin make format easy in neovim
+return {
+  "nvimdev/guard.nvim",
+  dependencies = {
+    "nvimdev/guard-collection",
+  },
+  opts = {
+    fmt_on_save = true,
+    lsp_as_default_formatter = true,
+  },
+  config = function(_, opts)
+    local ft = require('guard.filetype')
+
+    ft('javascript,typescript'):fmt('prettier'):lint('eslint')
+
+    require('guard').setup(opts)
+  end
+}
