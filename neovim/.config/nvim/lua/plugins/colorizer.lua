360de8edce7ba3a980e5f233a1427f18332c79e0
Alex Kornitzer <a.kornitzer@me.com>
2024-04-11 18:06:19 +0100

    neovim: remove colourizer

--- neovim/.config/nvim/lua/plugins/colorizer.lua
+++
@@ -1,7 +0,0 @@
--- Nvim-Colorizer: Maintained fork of the fastest Neovim colorizer
-return {
-  'NvChad/nvim-colorizer.lua',
-  config = function(_, opts)
-    require 'colorizer'.setup(opts)
-  end
-}
