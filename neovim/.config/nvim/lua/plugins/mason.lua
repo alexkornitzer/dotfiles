bf6a85358fee1c505cf3c2b4db1bdb26fca88092
Alex Kornitzer <akornitzer@volexity.com>
2024-07-13 16:08:50 +0100

    neovim: fixes, tweaks and updates

--- neovim/.config/nvim/lua/plugins/mason.lua
+++ neovim/.config/nvim/lua/plugins/mason.lua
@@ -12,31 +12,34 @@ return {
     require('mason-lspconfig').setup()
   end,
   init = function(_)
-    local pylsp = require("mason-registry").get_package("python-lsp-server")
-    pylsp:on("install:success", function()
-      local function mason_package_path(package)
-        local path = vim.fn.resolve(vim.fn.stdpath("data") .. "/mason/packages/" .. package)
-        return path
-      end
+    local registry = require("mason-registry")
+    if registry.has_package("python-lsp-server") then
+      local pylsp = registry.get_package("python-lsp-server")
+      pylsp:on("install:success", function()
+        local function mason_package_path(package)
+          local path = vim.fn.resolve(vim.fn.stdpath("data") .. "/mason/packages/" .. package)
+          return path
+        end
 
-      local path = mason_package_path("python-lsp-server")
-      local command = path .. "/venv/bin/pip"
-      local args = {
-        "install",
-        "-U",
-        "pylsp-mypy",
-        "pylsp-rope",
-        "python-lsp-isort",
-        "python-lsp-ruff",
-      }
+        local path = mason_package_path("python-lsp-server")
+        local command = path .. "/venv/bin/pip"
+        local args = {
+          "install",
+          "-U",
+          "pylsp-mypy",
+          "pylsp-rope",
+          "python-lsp-isort",
+          "python-lsp-ruff",
+        }
 
-      require("plenary.job")
-          :new({
-            command = command,
-            args = args,
-            cwd = path,
-          })
-          :start()
-    end)
+        require("plenary.job")
+            :new({
+              command = command,
+              args = args,
+              cwd = path,
+            })
+            :start()
+      end)
+    end
   end,
 }
