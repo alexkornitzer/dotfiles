a2274fad45381347ca01cd2d33bdd24f011aecd3
Alex Kornitzer <a.kornitzer@me.com>
2025-12-18 13:00:02 +0000

    nvim: don't use lsp for python formatting

--- neovim/.config/nvim/lua/plugins/guard.lua
+++ neovim/.config/nvim/lua/plugins/guard.lua
@@ -7,18 +7,9 @@ return {
   config = function(_, _)
     local ft = require('guard.filetype')
 
-    ft("python"):fmt({
-      fn = function(buf, range)
-        vim.lsp.buf.format({
-          buffer = buf,
-          range = range,
-          async = true,
-          name = "ruff",
-        })
-      end
-    })
     ft('javascript,typescript'):fmt('prettier')
     ft('json'):fmt('jq')
+    ft("python"):fmt('ruff')
     ft('yaml'):fmt('yamlfix'):env({ YAMLFIX_SECTION_WHITELINES = '1' })
 
     vim.g.guard_config = {
